b94a081ca7e409b0137fc4686bd1a1ad1aa1d9e7
HirokiObata <hiroki_obata@mail.toyota.co.jp>
2024-06-11 09:26:41 +0900

    新規作成

---
+++ src/main/kotlin/com/example/another_language/AnotherLanguageApplication.kt
@@ -0,0 +1,11 @@
+package com.example.another_language
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnotherLanguageApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnotherLanguageApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/another_language/AnotherLanguageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.another_language
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnotherLanguageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
